fd79bccbf8b894f07991a3615cc8e177863a0a1d
mateusz <mateusz.bzdzion@gmail.com>
2020-06-06 19:38:40 +0200

    Create urls for patient and doctor detail site

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from .views import index
+from .views import index, patient, doctor
 
 urlpatterns = [
     path('', index.IndexView.as_view(), name='index'),
+    path('patient/<int:pk>', patient.PatientDetailView.as_view(), name='patient_detail'),
+    path('doctor/<int:pk>', doctor.DoctorDetailView.as_view(), name='doctor_detail'),
+    
 ]
