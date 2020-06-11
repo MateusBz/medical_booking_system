61b1abaa7884d09914bc8fd0e5867e329e6ed3b2
mateusz <mateusz.bzdzion@gmail.com>
2020-06-11 10:39:51 +0200

    Fix views and signup_form

--- accounts/views/doctor.py
+++ accounts/views/doctor.py
@@ -11,10 +11,6 @@ class DoctorSignUpView(CreateView):
     form_class = DoctorSignUpForm
     template_name = 'accounts/signup_form.html'
 
-    def get_context_data(self, **kwargs):
-        kwargs['user_type'] = 'doctor'
-        return super().get_context_data(**kwargs)
-
     def form_valid(self, form):
         user = form.save()
         return redirect('login')
--- accounts/views/patient.py
+++ accounts/views/patient.py
@@ -11,10 +11,6 @@ class PatientSignUpView(CreateView):
     form_class = PatientSignUpForm
     template_name = 'accounts/signup_form.html'
 
-    def get_context_data(self, **kwargs):
-        kwargs['user_type'] = 'patient'
-        return super().get_context_data(**kwargs)
-
     def form_valid(self, form):
         user = form.save()
         return redirect('login')
