fc2670a58d2ff1c4aeb83032dc65d81251d80192
mateusz <mateusz.bzdzion@gmail.com>
2020-06-11 17:03:03 +0200

    Fix basedir

--- medical_booking_system/settings.py
+++ medical_booking_system/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'medical_booking_system.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'sqlite3.db',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     }
     # 'default': dj_database_url.config('DATABASE_URL')
     
