9e8d9af7f7ce236a3f3b17c52253004dfc4dcfa1
mateusz <mateusz.bzdzion@gmail.com>
2020-06-11 10:20:10 +0200

    Change max_length

--- accounts/models.py
+++ accounts/models.py
@@ -13,7 +13,7 @@ class Patient(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True)
     first_name = models.CharField(max_length=80)
     surname = models.CharField(max_length=80)
-    email = models.EmailField(max_length=100)
+    email = models.EmailField(max_length=80)
     pesel_number = models.CharField(max_length=11)
     phone = PhoneNumberField(max_length=15)
     day_of_birth = models.DateField(blank=True, null=True)
@@ -23,11 +23,11 @@ class Patient(models.Model):
         ('NN', 'Odmowa'),
     )
     gender = models.CharField(max_length=2, choices=GENDER)
-    street = models.CharField(max_length=100)
+    street = models.CharField(max_length=80)
     house_number = models.CharField(max_length=10)
     flat_number = models.CharField(max_length=10, blank=True, null=True)
     zip_code = models.CharField(max_length=6)
-    city = models.CharField(max_length=100)
+    city = models.CharField(max_length=80)
 
     def __str__(self):
         return self.first_name + ' ' + self.surname
@@ -50,7 +50,7 @@ class Doctor(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True)
     first_name = models.CharField(max_length=80)
     surname = models.CharField(max_length=80)
-    email = models.CharField(max_length=100)
+    email = models.CharField(max_length=80)
     phone = PhoneNumberField(max_length=15)
     medical_licence = models.CharField(max_length=7)
     is_active = models.BooleanField(default=False)
