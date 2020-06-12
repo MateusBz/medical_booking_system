45cb0c0960b57f489f194ffb5d4164d824494923
mateusz <mateusz.bzdzion@gmail.com>
2020-06-12 18:24:05 +0200

    Fix visit form

--- clinic/forms.py
+++ clinic/forms.py
@@ -1,10 +1,14 @@
 from django import forms
 
-from .models import Visit
+from .models import Visit, DoctorSchedule
 
 
 class VisitCreateForm(forms.ModelForm):
     
+    doctor = forms.ModelChoiceField(queryset=DoctorSchedule.objects.filter(occupied=False))
+
     class Meta:
         model = Visit
-        fields = ['doctor', 'date', 'time']
+        fields = ('doctor',)
+
+   
