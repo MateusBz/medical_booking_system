507049bb3f7fdcd4bd0e6e90782e638dbf178cba
mateusz <mateusz.bzdzion@gmail.com>
2020-06-12 18:23:08 +0200

    Create DoctorSchedule, Visit

--- clinic/models.py
+++ clinic/models.py
@@ -4,23 +4,20 @@ from django.contrib.auth import get_user_model
 from accounts.models import Doctor, Patient
 
 
+class DoctorSchedule(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.CharField(max_length=5)
+    occupied = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.doctor.surname + ' ' + self.doctor.first_name +' '+ self.date.strftime("%Y-%m-%d") +' '+ self.time
+
+
 class Visit(models.Model):
     patient = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
-    DATE = (
-        ('11.06', '11.06'),
-        ('12.06', '12.06')
-    )
-    date = models.CharField(max_length=5, choices=DATE)
-    TIMES = (
-        ('13:00', '13:00'),
-        ('13:30', '13.30'),
-        ('14:00', '14:00')
-    )
-    time = models.CharField(max_length=5, choices=TIMES)
-
+    doctor = models.OneToOneField(DoctorSchedule, on_delete=models.CASCADE)
     
-
     def get_absolute_url(self):
         return reverse("visit_detail", kwargs={"pk": self.pk})
     
