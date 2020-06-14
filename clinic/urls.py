f9309c2e9b0e52c72e37216c8f1275010e9cdcb3
mateusz <mateusz.bzdzion@gmail.com>
2020-06-14 12:14:55 +0200

    Add clinic/patient and clinic/doctor

--- clinic/urls.py
+++ clinic/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import VisitCreateView, VisitDetailView, VisitListView
+from .views import VisitCreateView, VisitDetailView, PatientVisitListView, DoctorVisitListView
 
 urlpatterns = [
     path('', VisitCreateView.as_view(), name='visit_creation'),
     path('<int:pk>/', VisitDetailView.as_view(), name='visit_detail'),
-    path('visits/', VisitListView.as_view(), name='visits_list'),
+    path('visits/patient/', PatientVisitListView.as_view(), name='visits_list'),
+    path('visits/doctor/', DoctorVisitListView.as_view(), name='doctor_visits_list'),
     ]
