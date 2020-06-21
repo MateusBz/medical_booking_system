e2975378260656ea627c84c8032c8260628db149
mateusz <mateusz.bzdzion@gmail.com>
2020-06-21 08:18:01 +0200

    Heroku 12

--- medical_booking_system/settings.py
+++ medical_booking_system/settings.py
@@ -83,12 +83,12 @@ WSGI_APPLICATION = 'medical_booking_system.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+    }
     # heroku
-    'default': dj_database_url.config('DATABASE_URL') 
+    # 'default': dj_database_url.config('DATABASE_URL') 
     
 }
 
