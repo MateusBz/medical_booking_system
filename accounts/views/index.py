8954f214b586ec6998937f1a7a4c7cc0ed235bb5
mateusz <mateusz.bzdzion@gmail.com>
2020-10-25 19:16:03 +0100

    add info about demo version

--- accounts/views/index.py
+++ accounts/views/index.py
@@ -4,5 +4,10 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["info"] = ["To jest demonstracyjna wersja aplikacji do zarządzania wizytami pacjentów i lekarzy. ", "Jeśli nie chcesz zakładać nowego konta użyj kont testowych:", "user: test_patient password: test_user", "user: test_doctor password: test_user"]
+        
+        return context
     
    
